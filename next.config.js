9055124bf6911bc1589fbd1e1a2d12c2dd22f728
Yaw Agyepong <yaw.agyepong@gmail.com>
2026-05-04 19:57:10 -0400

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn-images-1.medium.com', 'miro.medium.com', 'res.cloudinary.com'],
+  },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+}
