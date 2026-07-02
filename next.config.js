27fa812a68d2fd43dda93ed7e5a7eefcad0bb013
Yaw Agyepong <yaw.agyepong@gmail.com>
2026-07-02 13:14:45 -0400

    setting up github action

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
+
+const getBasePath = () => {
+  if(process.env.BASE_PATH) {
+    return `/${process.env.BASE_PATH}`
+  } else {
+    return undefined
+  }
+}
+
 module.exports = {
+  basePath: getBasePath(),
   reactStrictMode: true,
   images: {
     domains: ['cdn-images-1.medium.com', 'miro.medium.com', 'res.cloudinary.com'],
